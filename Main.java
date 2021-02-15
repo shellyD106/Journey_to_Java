9f3ac0896bd6fa5df6b4b261c4db5d487b96d291
shellyD106 <77341067+shellyD106@users.noreply.github.com>
2021-02-15 05:31:30 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package variableScope;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String firstName;
+		Integer classMembers;
+//		for( ; ; ) {
+//			System.out.println();
+//			
+//		}
+		if (true) {
+			int number = 5;
+		}
+		System.out.println(number);
+	}
+
+}
